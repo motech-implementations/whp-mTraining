e06ac1bed65c811d120258be65e422a99fea631f
Subhrajit Roy <sroy@thoughtworks.com>
2014-03-09 02:50:55 +0530

    Roy|13|Added some log statements and connection pooling as DBCP

--- src/main/java/org/motechproject/whp/mtraining/ivr/IVRResponseParser.java
+++ src/main/java/org/motechproject/whp/mtraining/ivr/IVRResponseParser.java
@@ -6,20 +6,26 @@ import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonParser;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.motechproject.whp.mtraining.exception.MTrainingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
 public class IVRResponseParser {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(IVRResponseParser.class);
+
     public IVRResponse parse(HttpResponse httpResponse) {
         try {
             String ivrResponseAsString = EntityUtils.toString(httpResponse.getEntity());
+            LOGGER.info(String.format("Attempting to parse IVR response %s", ivrResponseAsString));
             ObjectMapper objectMapper = new ObjectMapper();
             JsonFactory jsonFactory = objectMapper.getJsonFactory();
             JsonParser jsonParser = jsonFactory.createJsonParser(ivrResponseAsString);
             IVRResponse response = jsonParser.readValueAs(IVRResponse.class);
-            if(!response.hasValidationErrors()){
+            if (!response.hasValidationErrors()) {
                 response.markSuccess();
             }
             return response;
