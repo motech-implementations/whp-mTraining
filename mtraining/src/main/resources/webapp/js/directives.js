0793a417deaf57e7f66d6ebd5cb82713883cf6d2
Krzysztof Kaczmarczyk <kkaczmarczyk@soldevelo.com>
2014-08-05 16:48:20 +0200

    #8: added list of Courses jqgrid

--- mtraining/src/main/resources/webapp/js/directives.js
+++ mtraining/src/main/resources/webapp/js/directives.js
@@ -21,5 +21,80 @@
         };
     });
 
+    directives.directive('coursesGrid', function($http) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                var elem = angular.element(element), filters;
 
+                elem.jqGrid({
+                    url: '../mtraining/web-api/courses',
+                    datatype: 'json',
+                    jsonReader:{
+                        repeatitems:false,
+                        root: function (obj) {
+                            return obj;
+                        }
+                    },
+                    prmNames: {
+                        sort: 'sortColumn',
+                        order: 'sortDirection'
+                    },
+                    shrinkToFit: true,
+                    forceFit: true,
+                    autowidth: true,
+                    rownumbers: true,
+                    colNames: ['ID', scope.msg('mtraining.courseName'), scope.msg('mtraining.description'), scope.msg('mtraining.status'),
+                        scope.msg('mtraining.filename'), scope.msg('mtraining.dateCreated'), scope.msg('mtraining.lastUpdated')],
+                    colModel: [{
+                       name: 'id',
+                       index: 'id',
+                       hidden: true,
+                       key: true
+                    }, {
+                        name: 'name',
+                        index: 'name',
+                        align: 'center',
+                        width: 155
+                    }, {
+                        name: 'content',
+                        index: 'content',
+                        align: 'center',
+                        width: 200
+                    }, {
+                        name: 'state',
+                        index: 'state',
+                        align: 'center',
+                        width: 60
+                    },{
+                        name: 'content',
+                        index: 'content',
+                        align: 'center',
+                        width: 100
+                    }, {
+                        name: 'creationDate',
+                        index: 'creationDate',
+                        align: 'center',
+                        width: 70
+                    }, {
+                        name: 'modificationDate',
+                        index: 'modificationDate',
+                        align: 'center',
+                        width: 70
+                    }],
+                    width: '100%',
+                    height: 'auto',
+                    sortname: 'modificationDate',
+                    sortorder: 'desc',
+                    loadComplete : function(array) {
+                        $('.ui-jqgrid-htable').addClass('table-lightblue');
+                        $('.ui-jqgrid-btable').addClass("table-lightblue");
+                    },
+                    gridComplete: function () {
+                      elem.jqGrid('setGridWidth', '100%');
+                    }
+                });
+            }
+        };
+    });
 }());
